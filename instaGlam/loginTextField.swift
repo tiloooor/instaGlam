f00a1304514ea7391f31366d61feebcf98226deb
Tyler Holloway <htoyll@fb.com>
2017-06-28 14:10:35 -0700

    finished login

---
+++ instaGlam/loginTextField.swift
@@ -0,0 +1,21 @@
+//
+//  loginTextField.swift
+//  instaGlam
+//
+//  Created by Tyler Holloway on 6/28/17.
+//  Copyright © 2017 Tyler Holloway. All rights reserved.
+//
+
+import UIKit
+
+class loginTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ instaGlam/loginViewController.swift
@@ -0,0 +1,71 @@
+//
+//  loginViewController.swift
+//  instaGlam
+//
+//  Created by Tyler Holloway on 6/28/17.
+//  Copyright © 2017 Tyler Holloway. All rights reserved.
+//
+
+import UIKit
+import Parse 
+
+class loginViewController: UIViewController {
+    // MARK: Properties 
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loginButton.layer.cornerRadius = 20
+        loginButton.clipsToBounds = true
+        
+        registerButton.layer.cornerRadius = 20
+        registerButton.clipsToBounds = true
+
+    }
+    
+    @IBAction func pressRegisterButton(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("Yay, registered new user!")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func pressLoginButton(_ sender: Any) {
+        //someCode here
+        
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: Error?) in
+            if user != nil {
+                print("You're logged in!")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
